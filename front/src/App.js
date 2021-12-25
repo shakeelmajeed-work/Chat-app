e3b0b6acdfb1ef60a128d067256b10c4ed09e083
Shakeel Majeed <shakeelsmajeed@outlook.com>
2021-12-25 17:32:03 +0000

    Added new fonts, event acknowledgements for sending messages and location and fixed bug with send location button

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import './App.css';
-
+import './fonts/font.woff'
 
 function App() {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-      const newSocket = io(`http://${window.location.hostname}:3000`);
+      const newSocket = io(`http://${window.location.hostname}:3009`);
       setSocket(newSocket);
 
-      newSocket.on('welcome_message', (message) => {
+      newSocket.on('message', (message) => {
           console.log(message)
       })
+
+      //takes into account everything that has been submitted into the html form 
+      document.querySelector("#message-form").addEventListener('submit', (event) => { //submit gets confused with more and more buttons 
+        event.preventDefault()
+        const inp = event.target.elements.primary.value //a safer way as opposed to document.querySelector().value //target = form with id 'message-form'
+        newSocket.emit('send_message', inp, () => {//will turn this on server-side so that the message can be sent to all connected clients
+          console.log("Message was sent successfully")
+        }) 
+  
+      })
       
+      document.querySelector('#location').addEventListener('click', (event) => {
+        if (!navigator.geolocation) {
+          return alert('Your browser does not support the Gelocation API and cannot get your location')
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => { //using a callback func to pass on res from geolocation api to server-side
+          newSocket.emit('send_location', {latitude: position.coords.latitude, longitude: position.coords.longitude}, () => {
+            console.log("Your location has been shared!") //event acknowledgement output
+
+          })
+        })
+      })
+
     }, [setSocket]);
+    
     
     
   
     return ( //<!--gets data from server side (express) and fetches it to client-side (react) for rendering-->
-        <p>Hey</p> 
+        <div className='format-text'>
+          <h1>Chat App:</h1>
+          <form id="message-form">
+            <input name="primary" placeholder='Your message'></input>
+            <button className='format-text'>Send Message</button>
+            <br></br>  
+          </form>
+        <button id="location" className='format-text'>Send your Location</button>
+        </div>
     );
   }
   
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 //Line 5 of chat-app/front/package.json  = will let the webpack (something that allows changes to code to be immediately rendered) proxy the API requests to the backend Express
 const http = require('http');
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const express = require('express');
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server) //cannot pass in app here
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3009;
 
 //reminder about setting publicdirectorypath
 
 io.on('connection', (socket) => {
    console.log("New connection")
-
-   socket.emit('welcome_message',"Welcome!")
    
+   // ------Standard procedures when a user join ------- //
+   socket.emit('message',"Welcome!") //sends welcome message when user immediately joins the room
+   socket.broadcast.emit('message', "A new user has joined!") //everyone apart from the newly connected socket will know that someone new has joiend 
+
+   // ------Procedures based on if anyone wants to send a message ------- //
+   socket.on('send_message', (message, callback) => { //message is the input from the form
+      const filter = new Filter()
+      filtered_message = filter.clean(message)
+      
+      io.emit('message', filtered_message)
+      callback() //the anon function that deals with an event acknowledgement (in this case it will say that the message was delivered)
+   }) 
+   socket.on('send_location',(pos,callback) => {
+      io.emit('message', `One user is from https:google.com/maps?q=${pos.latitude},${pos.longitude}`)
+      callback()
+   })
+
+   // ------If and when a connected client disconnects---- //
+   socket.on('disconnect', () => {
+      io.emit('message', "A user has disconnected")
+   })
+
 })
 
 server.listen(port, () => console.log(`Listening on port ${port}`)); 
 
-app.get('/express_backend', (req,res) => { //a GET url route that the React front end will fetch
-   res.send({express:'Hey'}) //if this is show it shows that the express backend is connected to the react frontent
+app.get('/express_backend', (req,res) => { //a GET url route that the React front end can fetch
+   res.send({express:'Hey'}) //if this is shown it shows that the express backend is connected to the react frontent
 })
